398289a8702327dd01f3e8b88d7da8b581dacd22
OlenaKopachynska <kopachinskao99@gmail.com>
2021-11-08 16:50:19 +0200

    error handler changed

--- errors/ErrorHandler.js
+++ errors/ErrorHandler.js
@@ -2,8 +2,6 @@ class ErrorHandler extends Error {
     constructor(status, message = '', customCode, data = '') {
         super(message);
         this.status = status;
-        this.customCode = customCode;
-        this.data = data;
 
         Error.captureStackTrace(this, this.constructor);
     }
